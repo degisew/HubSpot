da8e7e0805384039cf20178118b2774905096c10
Degisew <jegisew21@gmail.com>
2023-05-04 17:32:28 +0300

    Configured postgresql database

--- discordClone/settings.py
+++ discordClone/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'discordClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':'localhost',
+        'USER':'postgres',
+        'PASSWORD': '1234',
+        'NAME':'Discord_Clone',
+        'PORT':'5432'
     }
 }
 
