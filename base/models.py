44045b72bd50699be637703f868f0cb5424b0f01
Degisew <jegisew21@gmail.com>
2023-05-07 12:41:40 +0300

    Created a Message Model

--- base/models.py
+++ base/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Room(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
     # participants = 
+
     updated_at =  models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -13,5 +14,12 @@ class Room(models.Model):
         return self.name
 
 class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.Foreignkey(Room, on_delete = models.CASCADE)
+    body = models.TextField()
 
-    room = models.Foreignkey(Room, on_delete = models.SET_NULL)
+    updated_at =  models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
