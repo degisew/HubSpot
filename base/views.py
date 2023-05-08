60b4ffc09a0cd5411b95b10e320102ed3275fd46
Degisew <jegisew21@gmail.com>
2023-05-08 23:27:28 +0300

    Define a new view for creating room

--- base/views.py
+++ base/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
-    return render(request, 'base/home.html', {'rooms': rooms})
+    return render(request, 'base/home.html', context)
 
 
 def room(request):
     return render (request, 'base/room.html')
+
+def create_room(request):
+    form = RoomForm()
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
