be01e15675249e42430fc0d6472f6b3881bc04a5
Degisew Mengist <jegisew21@gmail.com>
2023-10-13 20:51:07 +0300

    Created a room model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    #host
+    #topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    #participants
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
