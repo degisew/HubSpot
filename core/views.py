c1104a583448689033a0c8841c8a1caf02c390e5
Degisew Mengist <jegisew21@gmail.com>
2023-10-13 12:38:21 +0300

    Added a new template

--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-   return render(request, 'home.html')
+   return render(request, 'core/home.html')
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'core/room.html')
