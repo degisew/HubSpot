c70816ec16393e44ebf0cec7b8722261103fef32
Degisew Mengist <jegisew21@gmail.com>
2023-10-13 20:27:15 +0300

    Remove dead code

--- discord/settings.py
+++ discord/settings.py
@@ -78,13 +78,6 @@ WSGI_APPLICATION = 'discord.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
