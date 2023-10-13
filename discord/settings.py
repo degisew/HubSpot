5ca42cf2387236f8a2c7160eb493e836c7171d44
Degisew Mengist <jegisew21@gmail.com>
2023-10-13 20:20:52 +0300

    Connected with postgres

--- discord/settings.py
+++ discord/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'discord.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'NAME': 'discord',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
