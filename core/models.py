f39dd4c85bbe42616022508b21edc592e8595fe2
Degisew Mengist <jegisew21@gmail.com>
2023-10-14 14:43:46 +0300

    Create Relationship

--- core/models.py
+++ core/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+    
 class Room(models.Model):
-    #host
-    #topic
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     #participants
@@ -15,7 +21,7 @@ class Room(models.Model):
         return self.name
     
 class Message(models.Model):
-    #user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
