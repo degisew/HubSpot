c01594313118f8303ab6bf17765c1bf6c76c7109
Degisew Mengist <jegisew21@gmail.com>
2023-10-14 13:59:50 +0300

    Get data from the DB

--- core/views.py
+++ core/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
+from .models import Room
 
-
-rooms = [
-    {'id': 1, 'name': 'Python'},
-    {'id': 2, 'name': 'JavaScript'},
-    {'id': 3, 'name': 'Django'}
-]
 
 def home(request):
-   return render(request, 'core/home.html', {'rooms': rooms})
+    rooms = Room.objects.all()
+    return render(request, 'core/home.html', {'rooms': rooms})
 
 
 def room(request, pk):
-    name = None
-    for room in rooms:
-        if room['id'] == int(pk):
-            name = room['name']
-    return render(request, 'core/room.html', {'room': name})
+    room = Room.objects.get(id=pk)
+    return render(request, 'core/room.html', {'room': room})
