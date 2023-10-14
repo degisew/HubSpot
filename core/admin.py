696f82ebadc67677137f3d96df766aa35a59082c
Degisew Mengist <jegisew21@gmail.com>
2023-10-14 14:46:15 +0300

    Registered Topic model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Room
+from .models import Message, Room, Topic
 
 
 admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
 
