a3ca67d0efee494b8e93706e96214a2ff5e3ed9a
Degisew <jegisew21@gmail.com>
2023-12-25 22:21:57 +0300

    Added a counting logic

--- core/views.py
+++ core/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import redirect, render
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
     QS = request.GET.get('qs')
     query_string = QS if QS != None else ''
-    rooms = Room.objects.order_by('-created_at').select_related('topic').filter(topic__name__icontains=query_string)
+    rooms = Room.objects.order_by('-created_at').select_related('topic').filter(
+        Q(topic__name__icontains=query_string) |
+        Q(name__icontains=query_string) |
+        Q(description__icontains=query_string)
+        )
     topics = Topic.objects.all()
-    return render(request, 'core/home.html', {'rooms': rooms, 'topics': topics})
+    room_count = rooms.count()
+    return render(request, 'core/home.html', {'rooms': rooms, 'topics': topics, 'room_count': room_count})
+
 
 
 def room(request, pk):
