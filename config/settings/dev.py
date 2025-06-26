746abac2117bf22b2812c79b6550f458875ec3df
Degisew <jegisew21@gmail.com>
2025-06-26 16:57:11 +0300

    ref(App): dockerize the app and organize project structure

---
+++ config/settings/dev.py
@@ -0,0 +1,4 @@
+from config.settings.base import *  # noqa F403
+
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Message, Room, Topic
-
-
-admin.site.register(Room)
-admin.site.register(Message)
-admin.site.register(Topic)
-
